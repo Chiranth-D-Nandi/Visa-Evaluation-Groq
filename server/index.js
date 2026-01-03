1fa9e4430afb1df2054f6cec5fe18b23a9c2b430
Chiranth D Nandi <chiranthnandi@example.com>
2026-01-04 03:04:15 +0530

    Remove Express 5 incompatible options wildcard

--- server/index.js
+++ server/index.js
@@ -46,9 +46,6 @@ app.use(cors({
   exposedHeaders: ['Content-Length', 'Content-Type']
 }));
 
-// Handle preflight requests
-app.options('*', cors());
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
